da2f5abf2cbfebc56c4d781fe72a8faed3a2273c
ChenDa1994 <413799190@qq.com>
2016-02-28 16:37:24 +0800

    message

--- server-modules/weixin_event.js
+++ server-modules/weixin_event.js
@@ -16,14 +16,39 @@ function eventHandler(msg){
 
   if(eventKey == 'CHECK_IN'){
     //每日打卡
+    //扫描是否有文件
     baseResult.xml.Content = msgConfig.DAILY_CHECKIN_MSG(true);
-  }else if(eventKey == 'JOIN'){
+  }else if(eventKey == 'JOIN_CLASS'){
     //加入计划
+    saveUserObject(msg);
     baseResult.xml.Content = msgConfig.JOIN_MSG(true);
   }else{
     baseResult.xml.Content = msgConfig.UNKNOWN_MSG;
   }
   return baseResult;
+}
+
+function dailyCheckIn(msg, cb){
+  //1. 扫描是否有打卡记录
+  //  ~ 如果么有提示去打卡，否则不能打卡
+
+  //2. 如果有至少一条打卡记录，完成打卡
+
+
+}
+
+function saveUserObject(msg){
+  var openId = msg.xml.FromUserName[0];
+  var user = new AV.User();
+  user.set('username', openId);
+  user.set('password', 'f32@ds*@&dsa'); //默认密码,目前不做任何功能使用
+  user.signUp().then(function(user) {
+    // 注册成功，可以使用了
+    console.log(user);
+  }, function(error) {
+    // 失败了
+    console.log('Error: ' + error.code + ' ' + error.message);
+  });
 }
 
 //获取消息模板
